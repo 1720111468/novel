427ea58e1e14959f4350c6293cc1e221d38f57c6
1720111468 <1720111468@qq.com>
2020-02-13 22:04:12 +0800

    1

--- src/main/java/com/cy/controller/IndexController.java
+++ src/main/java/com/cy/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-	@RequestMapping("/page/{moduleName}")
+	@RequestMapping("/{moduleName}")
 	public String module(@PathVariable String moduleName){
 		
 		return moduleName;
